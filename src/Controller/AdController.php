ceaf55b0474b86640f5c93a104bbdb413f08e2f2
Dihamtn <hamdi.zamit@outlook.com>
2021-11-10 15:12:45 +0100

    projet 10/11

---
+++ src/Controller/AdController.php
@@ -0,0 +1,86 @@
+22:58 -4
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Ad;
+use App\Repository\AdRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AdController extends AbstractController
+{
+    /**
+     * @Route("/ads", name="ads_index")
+     */
+    public function index(AdRepository $repo): Response
+    {
+        $ads = $repo->findAll();
+        return $this->render('ad/index.html.twig', [
+            'ads' => $ads
+        ]);
+    }
+    /**
+     * Permet d'afficher une seule annonce
+     * 
+     * @Route("/ads/{slug}", name=ads_show"
+     * 
+     * @return Response
+     */
+    public function show {$slug}{
+        $repo = $this->getDoctrine()
+    }
+}
+
+namespace App\Controller;
+
+use App\Entity\Ad;
+use App\Repository\AdRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AdController extends AbstractController
+{
+    /**
+     * @Route("/ads", name="ads_index")
+     */
+    public function index(AdRepository $repo): Response
+    {
+        $ads = $repo->findAll();
+        return $this->render('ad/index.html.twig', [
+            'ads' => $ads
+        ]);
+    }
+}
+
+
+
+
+
+
+
+
+
+/**namespace App\Controller;
+
+use App\Entity\Ad;
+use App\Repository\AdRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AdController extends AbstractController
+{
+    /**
+     * @Route("/ads", name="ads_index")
+     */
+   /* public function index(AdRepository $repo): Response
+    {
+        $ads = $repo->findAll();
+        return $this->render('ad/index.html.twig', [
+            'ads' => $ads
+        ]);
+    }
+}
